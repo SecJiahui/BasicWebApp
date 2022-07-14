33f6f0443819872e9950e6a7a2e12332797ce45f
SecJiahui <87092626+SecJiahui@users.noreply.github.com>
2022-07-14 15:43:00 +0200

    test plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,6 +1,7 @@
 package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
+import org.thymeleaf.util.StringUtils;
 
 @Service
 public class QueryProcessor {
@@ -13,8 +14,16 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.contains("following numbers is the largest")) { //
+            // TODO extend the programm here
+
+         //   String[] newStr = StringUtils.split(query);
             return "";
+        } else if (query.contains("plus")) {
+            String[] newStr = query.split("\\s+");
+            return newStr[3] + newStr [5];
+        } else {
+            return "Sorry, it is too hard for me";
         }
     }
 }
